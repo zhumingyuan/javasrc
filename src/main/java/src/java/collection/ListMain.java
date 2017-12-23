1a14f6b638880de70f8b2867ffcb44b2ad1a964e
zhumingyuan <461624128@qq.com>
2017-12-23 17:29:27 +0800

    java source code collection 1

---
+++ src/main/java/src/java/collection/ListMain.java
@@ -0,0 +1,11 @@
+package src.java.collection;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListMain {
+
+    List<String> arrayList = new ArrayList<>();
+    List<String> linkedList = new LinkedList<>();
+}
---
+++ src/main/java/src/java/collection/MapMain.java
@@ -0,0 +1,13 @@
+package src.java.collection;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MapMain {
+
+    Map<String,String> hashMap = new HashMap<>();
+    Map<String,String> linkedHashMap = new LinkedHashMap<>();
+    Map<String,String> treeMap = new TreeMap<>();
+ }
